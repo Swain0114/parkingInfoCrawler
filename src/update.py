d478ab3fdca501c959df66dab527c98ac2181a8c
Bob <swain.tseng@wemoscooter.com>
2017-12-31 10:08:48 +0800

    新增更新star

--- src/update.py
+++ src/update.py
@@ -4,20 +4,24 @@ from controller import *
 res = requests.get("http://data.tycg.gov.tw/opendata/datalist/datasetMeta/download?id=f4cc0b12-86ac-40f9-8745-885bddc18f79&rid=0daad6e6-0632-44f5-bd25-5e1de1e9146f")
 data = res.json()
 sched = BlockingScheduler()
-# 將資料寫至local file
-# fd = open(path + str(now) + '-ParkingLotInfo.json', 'w')
-# json.dump(data,fd,indent=1,ensure_ascii=False)
-# pprint.pprint(data)
-# query = "SELECT * FROM ParkingInfo"
-# pprint.pprint(getMysqlData(gcp_cur_mysql,query))
+
+
 @sched.scheduled_job('interval', minutes=2)
 def timed_job():
 	# 因架設在heroku上，時區需要+8
+	query = "SELECT parkId, avg(star) as avgStar FROM comment GROUP BY parkid"
+	star = getMysqlData(lab_cur_mysql,query)
 	now = datetime.datetime.now() + datetime.timedelta(hours=8)
 	now = now.strftime("%Y-%m-%d %H:%M:%S")
 	for row in data['parkingLots']:
 		# print(row)
-		query = "UPDATE ParkingInfo SET surplusSpace = '%s', update_dt = '%s' WHERE parkId = '%s'"%(row['surplusSpace'], now ,row['parkId'])
+		for rank in star:
+			if rank['parkId'] == row['parkId']:
+				row['star'] = float(rank['avgStar'])
+		if len(row) == 12:
+			query = "UPDATE ParkingInfo SET surplusSpace = '%s', star = '%s' ,update_dt = '%s' WHERE parkId = '%s'"%(row['surplusSpace'], row['star'] ,now ,row['parkId'])
+		else:
+			query = "UPDATE ParkingInfo SET surplusSpace = '%s', update_dt = '%s' WHERE parkId = '%s'"%(row['surplusSpace'], now ,row['parkId'])
 		print(query)
 		msg = insertMysql(lab_cur_mysql, query)
 
